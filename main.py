8ddbe3cee081268625bd0e994a5d954b86a0290d
jtdev632 <jaccek.bid@gmail.com>
2024-08-19 22:36:39 +0200

    .

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-# Load the dataset
+
 data = pd.read_csv('fixed_user_preferences.csv')
 print("Loading dataset {}".format(data.keys()))
 
-# Preprocess the da
 
