885e67640930b5971c671f0d351d5a74b6c69fa8
jtdev632 <jaccek.bid@gmail.com>
2024-08-21 22:20:02 +0200

    add Random Forests model

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import numpy as np
 data = pd.read_csv('encoded_100_users_data.csv')
 
-# Separate features (X) and target (y)a
+
 X_train, X_test, y_train, y_test = train_test_split(
     data.drop('Owns_MacBook', axis=1), data['Owns_MacBook'], random_state=0
 )
-knn = KNeighborsClassifier(n_neighbors=1)
+knn = RandomForestClassifier()
 knn.fit(X_train, y_train)
 X_new = np.array([[65,0,0,1,0]])
 predictions = knn.predict(X_new)
