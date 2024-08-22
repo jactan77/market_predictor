6f70878f9ad8040b2783c11428868304e9a5c161
jtdev632 <jaccek.bid@gmail.com>
2024-08-22 21:11:15 +0200

    add DecisionTreeClassifier

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.tree import export_graphviz
+import graphviz
+
 data = pd.read_csv('encoded_100_users_data.csv')
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    data.drop('Owns_MacBook', axis=1), data['Owns_MacBook'], random_state=0
+    data.drop('Owns_MacBook', axis=1), data['Owns_MacBook'], random_state=42
 )
-knn = RandomForestClassifier()
-knn.fit(X_train, y_train)
-X_new = np.array([[65,0,0,1,0]])
-predictions = knn.predict(X_new)
-print('X_new shape: {}'.format(X_new.shape))
-print('predictions: {}'.format(
-    [predictions][0]
-))
-y_pred = knn.predict(X_test)
-print("Score: {:.2f}".format(knn.score(X_test,y_test)))
+tree = DecisionTreeClassifier(max_depth=4, random_state=0)
+tree.fit(X_train, y_train)
+print("Score test: {:.2f}".format(tree.score(X_test,y_test)))
+print("Score train: {:.2f}".format(tree.score(X_train, y_train)))
 
+export_graphviz(tree, out_file='tree.dot', feature_names=data.drop('Owns_MacBook', axis=1).columns,
+                class_names=['No', 'Yes'], rounded=True, filled=True)
+
+with open("tree.dot") as f:
+    dot_graph = f.read()
+graphviz.Source(dot_graph)
